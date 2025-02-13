50d79dbbc4f4f2652d3dcd264a271657c543294b
dev-deg <david.deguara@mcast.edu.mt>
2025-02-13 12:09:28 +0100

    Added basic PCG terrain generation

--- Assets/Scripts/BasicPlatformer/Generator/BasicPlatformerGenerator.cs
+++ Assets/Scripts/BasicPlatformer/Generator/BasicPlatformerGenerator.cs
@@ -45,6 +45,7 @@ namespace BasicPlatformer.Generators
             // Step 2. Determine Corner Placement
 
             // Step 3. Render the terrain
+            RenderTiles(heights, null);
         }
 
         private int[] GenerateHeights()
@@ -55,7 +56,7 @@ namespace BasicPlatformer.Generators
             
             int minSurface = settings.MinSurfaceHeight;
             int maxSurface = settings.MaxSurfaceHeight;
-            int maxHeightVariations = settings.MaxHeightVariation;
+            int maxHeightVariation = settings.MaxHeightVariation;
             int minSectionWidth = settings.MinSectionWidth;
 
             int currentHeight = rng.Next(minSurface, maxSurface + 1);
@@ -64,8 +65,48 @@ namespace BasicPlatformer.Generators
             while (i < width)
             {
                 //TODO: Generate the heights
+                int remaining = width - i;
+                
+                //Generate a random Width
+                int sectionWidth = (remaining < minSectionWidth)
+                    ? remaining
+                    : rng.Next(minSectionWidth, Math.Min(minSectionWidth + 3, remaining) + 1);
+                //Generate a random Height
+                if (i > 0)
+                {
+                    int delta = rng.Next(-maxHeightVariation, maxHeightVariation + 1);
+                    currentHeight = Mathf.Clamp(heights[i - 1] + delta, minSurface, maxSurface);
+                }
+
+                for (int j = 0; j < sectionWidth && i < width; j++)
+                {
+                    heights[i] = currentHeight;
+                    i++;
+                }
             }
             return heights;
+        }
+
+        private void RenderTiles(int[] heights, TopTileType[] topTileTypes)
+        {
+            int width = settings.Width;
+            int dirtDepth = settings.DirtDepth;
+
+            for (int x = 0; x < width; x++)
+            {
+                int H = heights[x];
+                TileBase topTile = settings.GrassTile;
+                
+                //Place the top grass tile
+                Vector3Int pos = new Vector3Int(x, H, 0);
+                groundTilemap.SetTile(pos, topTile);
+                
+                //Place dirt tiles beneath it
+                for (int y = H - 1; y >= H - dirtDepth; y--)
+                {
+                    groundTilemap.SetTile(new Vector3Int(x,y,0),settings.DirtTile);
+                }
+            }
         }
         
         [Button("Reset Terrain")]
